d0e58453a75d47760f5c2eacc2b41cf5b6c307d4
HoeSeong123 <ghltjd3@gmail.com>
2023-03-16 08:21:16 +0900

    p120833 배열 자르기

---
+++ src/main/java/com/ll/p120833/p120833.java
@@ -0,0 +1,16 @@
+package com.ll.p120833;
+
+public class p120833 {
+
+}
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2-num1+1];
+        for(int i = 0; i < num2 - num1 + 1; i++) {
+            answer[i] = numbers[num1 + i];
+        }
+
+        return answer;
+    }
+}
